eb3309a4c1f95b58e022f7f5ce449f6467d40fb3
FAIRY <869659010@qq.com>
2025-11-25 23:33:27 +0800

    Complete the RideInterface according to part2

--- src/RideInterface.java
+++ src/RideInterface.java
@@ -1,2 +1,10 @@
 public interface RideInterface {
+    void addVisitorToQueue(Visitor visitor);
+    Visitor removeVisitorFromQueue();
+    void printQueue();
+    void addVisitorToHistory(Visitor visitor);
+    boolean checkVisitorFromHistory(Visitor visitor);
+    int numberOfVisitors();
+    void printRideHistory();
+    void runOneCycle();
 }
